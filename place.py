5c44cf3936b7086a1102f0790128cdf1bb9a6878
Ashwin <35029567+ashwin-r-k@users.noreply.github.com>
2023-02-10 23:13:19 +0530

    Update place.py
    something

--- place.py
+++ place.py
@@ -30,22 +30,8 @@ sety(-170)
 end_fill()
 red = Turtle() # Assigning "Red" as name of the turtle
 
-def curve(): # Method to draw curve
-    for i in range(200): # To draw the curve step by step
-        red.right(1)
-        red.forward(1)
+red.right(1)
+red.forward(1)
+red.forward(15)
 
-def heart():  # Method to draw full Heart
-    red.fillcolor('red')
-    red.begin_fill()
-    red.left(140)
-    red.forward(113)
-    curve() # Left Curve
-    red.left(120)
-    curve() # Right Curve
-    red.forward(112)
-    red.end_fill()
-
-heart()
-red.ht() # Hiding Turtle
 turtle.done()

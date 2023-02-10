db03756dedd767594effb445815e370124243331
Antony Joseph Mathew <48788499+ajmanjoma@users.noreply.github.com>
2023-02-11 00:15:47 +0530

    Update place.py

--- place.py
+++ place.py
@@ -43,6 +43,32 @@ begin_fill()
 circle(23)
 end_fill()
 
+
+
+
+
+
+~AnJoMA~
+t = turtle.Turtle()
+s = 50
+# drawing first side
+t.forward(s) # Forward turtle by s units
+t.left(90) # Turn turtle by 90 degree
+ 
+# drawing second side
+t.forward(s) # Forward turtle by s units
+t.left(90) # Turn turtle by 90 degree
+ 
+# drawing third side
+t.forward(s) # Forward turtle by s units
+t.left(90) # Turn turtle by 90 degree
+ 
+# drawing fourth side
+t.forward(s) # Forward turtle by s units
+t.left(90) # Turn turtle by 90 degree
+~end~
+
+
 # my code
 
 forward(20)

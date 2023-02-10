8f5b5afa81724a846ff13cdf807b037334712b64
Himanshu <53328455+Himanshudewangan6973@users.noreply.github.com>
2023-02-11 00:10:56 +0530

    Update place.py

--- place.py
+++ place.py
@@ -1,4 +1,5 @@
 from turtle import *
+import turtle
 import random
 
 Screen().setup(width=1.0, height=1.0)
@@ -29,59 +30,61 @@ setx(0)
 sety(-170)
 end_fill()
 
-fillcolor('#00ff00')
-begin_fill()
-penup()
-sety(-17)
-setpos((-64, 5))
-setx(16)
-setpos((0, -7))
-end_fill()
+tur=turtle.Turtle()
+tur.fillcolor('#f2d840')
+tur.begin_fill()
+tur.circle(100)
+tur.end_fill()
+tur.up()
 
-fillcolor('#08fee5')
-begin_fill()
-circle(23)
-end_fill()
+tur.goto(-40, 120)
+tur.down()
+tur.fillcolor('white')
+tur.begin_fill()
+tur.circle(18)
+tur.end_fill()
+tur.up()
 
-# my code
+tur.goto(-37, 125)
+tur.down()
+tur.fillcolor('black')
+tur.begin_fill()
+tur.circle(7)
+tur.end_fill()
+tur.up()
 
-forward(20)
-right(200)
-forward(260)
+tur.goto(40, 120)
+tur.down()
+tur.fillcolor('white')
+tur.begin_fill()
+tur.circle(18)
+tur.end_fill()
+tur.up()
 
+tur.goto(40, 125)
+tur.down()
+tur.fillcolor('black')
+tur.begin_fill()
+tur.circle(7)
+tur.end_fill()
+tur.up()
 
-# ------- -------- #
-#  Nee shooperada! #
-#------------------#
-pen = Turtle()
-pen.color("black")
-pen.pensize(3)
-pen.speed(10)
-pen.hideturtle()
-pen.penup()
-pen.goto(-500,100)
-pen.pendown()
-pen.write("                       _                                      _", align="left", font=("monospace", 16, "bold"))
-pen.penup()
-pen.goto(-500,80)
-pen.pendown()
-pen.write(" _ __   ___  ___   ___| |__   ___   ___  _ __   ___ _ __ __ _  __| | __ _ ", align="left", font=("monospace", 16, "bold"))
-pen.penup()
-pen.goto(-500,60)
-pen.pendown()
-# ------- -------- #
-#      end         #
-# nee shooperada   #
-#------------------#
+tur.goto(-40, 85)
+tur.down()
+tur.right(90)
+tur.circle(40, 180)
+tur.up()
 
+tur.goto(-40, 85)
+tur.down()
+tur.right(136)
+tur.circle(56, 90)
+tur.up()
 
-#Ashwin
-red =Turtle()
+tur.goto(-450, -150)
+tur.write("FOSSMEET'23...", font=("Indie Flower",30))
 
-red.right(1)
-red.forward(2)
-red.right(2)
-red.forward(2)
-red.right(2)
-red.forward(2)
-red.right(2)
+tur.goto(150, -150)
+turtle.speed(3)
+tur.write("HEY, KEEP SMILING...", font=("Indie Flower",30))
+exitonclick()

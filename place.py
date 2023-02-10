7b90882cfaa1b08d87c927ccaf10b83929c11bb8
Himanshu <53328455+Himanshudewangan6973@users.noreply.github.com>
2023-02-11 00:17:11 +0530

    Update place.py

--- place.py
+++ place.py
@@ -1,5 +1,4 @@
 from turtle import *
-import turtle
 import random
 
 Screen().setup(width=1.0, height=1.0)
@@ -29,6 +28,81 @@ sety(-512)
 setx(0)
 sety(-170)
 end_fill()
+
+fillcolor('#00ff00')
+begin_fill()
+penup()
+sety(-17)
+setpos((-64, 5))
+setx(16)
+setpos((0, -7))
+end_fill()
+
+fillcolor('#08fee5')
+begin_fill()
+circle(23)
+end_fill()
+
+# my code
+
+forward(20)
+right(200)
+forward(260)
+
+
+# ------- -------- #
+#  Nee shooperada! #
+#------------------#
+pen = Turtle()
+pen.color("black")
+pen.pensize(3)
+pen.speed(10)
+pen.hideturtle()
+pen.penup()
+pen.goto(-500,100)
+pen.pendown()
+pen.write("                       _                                      _", align="left", font=("monospace", 16, "bold"))
+pen.penup()
+pen.goto(-500,80)
+pen.pendown()
+pen.write(" _ __   ___  ___   ___| |__   ___   ___  _ __   ___ _ __ __ _  __| | __ _ ", align="left", font=("monospace", 16, "bold"))
+pen.penup()
+pen.goto(-500,60)
+pen.pendown()
+pen.write("| '_ \\ / _ \\ / _ \\ / __| '_ \\ / _ \\ / _ \\| '_ \\ / _ \\ '__/ _` |/ _` |/ _` |", align="left", font=("monospace", 16, "bold"))
+pen.penup()
+pen.goto(-500,40)
+pen.pendown()
+pen.write("| | | |  __/  __/ \\__ \\ | | | (_) | (_) | |_) |  __/ | | (_| | (_| | (_| |", align="left", font=("monospace", 16, "bold"))
+pen.penup()
+pen.goto(-500,20)
+pen.pendown()
+pen.write("|_| |_|\\___|\\___| |___/_| |_|\\___/ \\___/| .__/ \\___|_|  \\__,_|\\__,_|\\__,_|", align="left", font=("monospace", 16, "bold"))
+pen.penup()
+pen.goto(-500,0)
+pen.pendown()
+pen.write("                                        |_|                               ", align="left", font=("monospace", 16, "bold"))
+
+# ------- -------- #
+#      end         #
+# nee shooperada   #
+#------------------#
+
+
+#Ashwin
+red =Turtle()
+
+red.right(1)
+red.forward(2)
+red.right(2)
+red.forward(2)
+red.right(2)
+red.forward(2)
+red.right(2)
+
+
+# Himanshu Dewangan
+import turtle
 
 tur=turtle.Turtle()
 tur.fillcolor('#f2d840')
@@ -88,3 +162,7 @@ tur.goto(150, -150)
 turtle.speed(3)
 tur.write("HEY, KEEP SMILING...", font=("Indie Flower",30))
 exitonclick()
+
+# End
+# Himanshu Dewangan
+# -----------------------------------------------------------

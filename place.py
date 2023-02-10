d4d7600eabf29864be91faaa846f94268c02ac4e
Cliford <62019276+clifordjoshy@users.noreply.github.com>
2023-02-10 15:27:34 +0530

    the big bang

--- place.py
+++ place.py
@@ -2,12 +2,29 @@ from turtle import *
 import random
 
 Screen().setup(width=1.0, height=1.0)
+speed(0)
 
-forward(20)
-circle(20)
-circle(2)
-circle(4)
-circle(5)
-forward(30)
-circle(20)
-circle(20)
+fillcolor('#1793d1')
+begin_fill()
+penup()
+sety(-170)
+setpos((-640, 512))
+setx(640)
+setpos((0, -170))
+end_fill()
+
+fillcolor('#d70a53')
+begin_fill()
+sety(-512)
+setx(640)
+sety(512)
+end_fill()
+
+setx(-640)
+
+fillcolor('#072c61')
+begin_fill()
+sety(-512)
+setx(0)
+sety(-170)
+end_fill()

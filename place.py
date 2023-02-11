140d160f82378324d6a79dcab44951b2622356e0
inamurahman <inamvkd@gmail.com>
2023-02-11 06:52:44 +0530

    batman

--- place.py
+++ place.py
@@ -258,3 +258,77 @@ left(135)
 forward(20)
 left(45)
 forward(20)
+
+
+turt = Turtle()
+ 
+turt.turtlesize(1, 1, 1)
+turt.pensize(5)
+turt.penup()
+turt.goto(-270, 300)
+
+turt.color("black")
+turt.pendown()
+ 
+#turn1
+turt.left(90)   
+turt.circle(50, 85) 
+turt.circle(15, 110)
+turt.right(180) 
+ 
+#turn 2
+turt.circle(30, 150)
+turt.right(5)
+turt.forward(10) #draw forward line of 10 units
+ 
+#turn 3
+turt.right(90)
+turt.circle(-70, 140)
+turt.forward(40)
+turt.right(110)
+ 
+#turn 4
+turt.circle(100, 30)
+turt.circle(30, 100)
+turt.left(50)
+turt.forward(50)
+turt.right(145)
+ 
+#turn5
+turt.forward(30)
+turt.left(55)
+turt.forward(10)
+ 
+#reverse
+ 
+#turn 5
+turt.forward(10)
+turt.left(55)
+turt.forward(30)
+ 
+#turn 4
+ 
+turt.right(145)
+turt.forward(50)
+turt.left(50)
+turt.circle(30, 100)
+turt.circle(100, 30)
+ 
+#turn 3
+turt.right(90)
+turt.right(20)
+turt.forward(40)
+turt.circle(-70, 140)
+ 
+#turn 2
+turt.right(90)
+turt.forward(10)
+turt.right(5)
+turt.circle(30, 150)
+ 
+#turn 1
+turt.left(180)
+turt.circle(15, 110)
+turt.circle(50, 85)
+ 
+done()

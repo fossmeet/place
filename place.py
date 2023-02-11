4952f2239e523f63cf0913651f19a9b6a27334b3
Rithas K <rithask@icloud.com>
2023-02-11 05:51:10 +0530

    Change pen color for visibility

--- place.py
+++ place.py
@@ -182,6 +182,7 @@ draw_star(10, 300, 100)
 
 # Rithas K
 # Sorry for the long lines :)
+color('white')
 home()
 penup()
 goto(320, -170)
@@ -257,3 +258,5 @@ left(135)
 forward(20)
 left(45)
 forward(20)
+
+done()

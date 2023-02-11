d9155832e64a29d8ea2fa0e3d41e7842af557d04
Ashwin <35029567+ashwin-r-k@users.noreply.github.com>
2023-02-12 02:14:52 +0530

    H #5
    #5

--- place.py
+++ place.py
@@ -138,6 +138,24 @@ pen.goto(-500,-20)
 pen.pendown()
 #pen.write("  /$$$$$$            /$$                     /$$          ",align="left", font=("monospace", 8, "normal"))   
 
+red = Turtle()
+def curve(): # Method to draw curve
+    for i in range(200): # To draw the curve step by step
+        red.right(1)
+        red.forward(1)
+
+def heart():  # Method to draw full Heart
+
+    red.begin_fill()
+    red.left(140)
+    red.forward(113)
+    curve() # Left Curve
+    red.left(120)
+    curve() # Right Curve
+    red.forward(112)
+
+heart()
+
 """
 Want to write this
  /$$$$$$            /$$                     /$$          

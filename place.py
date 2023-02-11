5da06b6fa5d2cc3e41f9f57108442d98a6572f4d
PRASAD P <106724618+pr454d-p@users.noreply.github.com>
2023-02-11 23:12:27 +0530

    Update place.py

--- place.py
+++ place.py
@@ -1,51 +1,18 @@
-import turtle as t
-pen = t.Turtle()
-t.Screen().setup(width=1.0, height=1.0)
-t.speed(0)
-t.fillcolor('#1793d1')
-t.begin_fill()
-t.penup()
-t.sety(-170)
-t.setpos((-640, 512))
-t.setx(640)
-t.setpos((0, -170))
-t.end_fill()
+from turtle import *
+import random
+tracer(False)
+Screen().setup(width=1.0, height=1.0)
+speed(0)
 
-t.fillcolor('#d70a53')
-t.begin_fill()
-t.sety(-512)
-t.setx(640)
-t.sety(512)
-t.end_fill()
+fillcolor('#1793d1')
+begin_fill()
+penup()
+sety(-170)
+setpos((-640, 512))
+setx(640)
+setpos((0, -170))
+end_fill()
 
-t.setx(-640)
-
-t.fillcolor('#072c61')
-t.begin_fill()
-t.sety(-512)
-t.setx(0)
-t.sety(-170)
-t.end_fill()
-pen.color('#ffe4e1')
-pen.begin_fill()
-pen.left(42)
-pen.forward(120)
-pen.circle(80, 190)
-pen.right(100)
-pen.circle(80, 180)
-pen.forward(160)
-pen.left(90)
-pen.forward(50)
-pen.setpos(-60, 100)
-pen.end_fill()
-def txt():
-    pen.up()
-    pen.setpos(-60, 100)
-    pen.color('red')
-    pen.write("FOSSMEET'23", font=("Comic Sans MS", 16))
-txt()
-pen.end_fill()
-t.exitonclick()
 fillcolor('#d70a53')
 begin_fill()
 sety(-512)
@@ -76,6 +43,25 @@ begin_fill()
 circle(23)
 end_fill()
 
+##madhav
+t = Turtle()
+t.color("yellow")
+t.penup()
+t.setpos(100,100)
+t.begin_fill()
+
+t.circle(40, 300)
+t.left(90)
+t.forward(40)
+t.right(120)
+t.forward(40)
+t.end_fill()
+
+t.penup()
+
+
+t.hideturtle()
+##endmadhav
 
 
 # spidy
@@ -104,7 +90,7 @@ forward(260)
 pen = Turtle()
 pen.color("black")
 pen.pensize(3)
-pen.speed(10)
+pen.speed(0)
 pen.hideturtle()
 pen.penup()
 pen.goto(-500,100)
@@ -137,27 +123,32 @@ pen.write("                                        |_|                          
 #------------------#
 
 
-# Debian - Spiral Helix
-colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
-def create_spiral_helix():
-    setx(0)
-    sety(0)
-    speed(0)
-    for i in range(10):
-        fillcolor(colors[i%6])
-        begin_fill()
-        circle(5*i)
-        circle(-5*i)
-        left(i)
-        end_fill()
-    speed(5)
+#LEAF VILLAGE MWUHAHAHA
 
-create_spiral_helix()
-if __name__ == "__main__":
-    create_spiral_helix()
-else:
-    create_spiral_helix()
+a = Turtle()
+a.penup()
+a.goto(0,300)
+a.pendown()
+a.width(5)
 
+
+for i in range(38):
+    a.forward(i)
+    a.right(20)
+
+a.left(90)
+a.forward(100)
+a.penup()
+a.goto(-100,300)
+a.pendown()
+a.right(150)
+a.forward(100)
+a.left(113)
+a.forward(100)
+
+a.penup()
+a.goto(0,0)
+a.pendown()
 
 #Ashwin
 red =Turtle()
@@ -170,6 +161,11 @@ red.right(2)
 red.forward(2)
 red.right(2)
 
+setpos(100,100)
+fillcolor('red')
+begin_fill()
+circle(34)
+end_fill()
 
 #Alen
 def draw_star(size, x, y):
@@ -181,6 +177,512 @@ def draw_star(size, x, y):
         forward(size)
         right(144)
 
-speed("fastest")
-
 draw_star(10, 300, 100)
+
+# Rithas K
+# Sorry for the long lines :)
+color('white')
+home()
+penup()
+goto(320, -170)
+pendown()
+left(60)
+forward(100)
+right(90)
+forward(75)
+right(90)
+forward(20)
+right(90)
+forward(50)
+left(90)
+forward(20)
+left(90)
+forward(50)
+right(90)
+forward(20)
+right(90)
+forward(50)
+left(90)
+forward(40)
+right(90)
+forward(25)
+
+left(135)
+forward(20)
+left(45)
+forward(25)
+left(135)
+forward(20)
+
+left(180)
+forward(20)
+left(135)
+forward(40)
+left(45)
+forward(20)
+
+left(180)
+forward(20)
+left(45)
+forward(50)
+left(135)
+forward(20)
+
+left(180)
+forward(20)
+left(135)
+forward(20)
+left(45)
+forward(20)
+
+penup()
+goto(382, -114)
+pendown()
+
+left(180)
+forward(20)
+right(45)
+forward(5)
+
+left(180)
+forward(5)
+right(90)
+forward(50)
+left(135)
+forward(20)
+
+left(180)
+forward(20)
+left(135)
+forward(20)
+left(45)
+forward(20)
+
+penup()
+
+Turtle()
+ 
+home()
+pensize(5)
+penup()
+goto(-270, 300)
+
+color("black")
+pendown()
+ 
+#turn1
+left(90)   
+circle(50, 85) 
+circle(15, 110)
+right(180) 
+ 
+#turn 2
+circle(30, 150)
+right(5)
+forward(10) #draw forward line of 10 units
+ 
+#turn 3
+right(90)
+circle(-70, 140)
+forward(40)
+right(110)
+ 
+#turn 4
+circle(100, 30)
+circle(30, 100)
+left(50)
+forward(50)
+right(145)
+ 
+#turn5
+forward(30)
+left(55)
+forward(10)
+ 
+#reverse
+ 
+#turn 5
+forward(10)
+left(55)
+forward(30)
+ 
+#turn 4
+ 
+right(145)
+forward(50)
+left(50)
+circle(30, 100)
+circle(100, 30)
+ 
+#turn 3
+right(90)
+right(20)
+forward(40)
+circle(-70, 140)
+ 
+#turn 2
+right(90)
+forward(10)
+right(5)
+circle(30, 150)
+ 
+#turn 1
+left(180)
+circle(15, 110)
+circle(50, 85)
+
+penup()
+#dracu
+fillcolor('#d70a53')
+begin_fill()
+circle(23)
+end_fill()
+
+
+# Himanshu Dewangan
+
+tur=Turtle()
+tur.fillcolor('#f2d840')
+tur.begin_fill()
+tur.circle(100)
+tur.end_fill()
+tur.up()
+
+tur.goto(-40, 120)
+tur.down()
+tur.fillcolor('white')
+tur.begin_fill()
+tur.circle(18)
+tur.end_fill()
+tur.up()
+
+tur.goto(-37, 125)
+tur.down()
+tur.fillcolor('black')
+tur.begin_fill()
+tur.circle(7)
+tur.end_fill()
+tur.up()
+
+tur.goto(40, 120)
+tur.down()
+tur.fillcolor('white')
+tur.begin_fill()
+tur.circle(18)
+tur.end_fill()
+tur.up()
+
+tur.goto(40, 125)
+tur.down()
+tur.fillcolor('black')
+tur.begin_fill()
+tur.circle(7)
+tur.end_fill()
+tur.up()
+
+tur.goto(-40, 85)
+tur.down()
+tur.right(90)
+tur.circle(40, 180)
+tur.up()
+
+tur.goto(-40, 85)
+tur.down()
+tur.right(136)
+tur.circle(56, 90)
+tur.up()
+
+# End
+# Himanshu Dewangan
+
+#fedora logo
+size=40
+pensize(0.4*size)
+large_circle = size*2
+small_circle = 0.65*size
+center=(250,-300-large_circle)
+
+goto(center[0], center[1])
+setheading(0)
+color('white')
+penup()
+pendown()
+color('#294172')
+begin_fill()
+circle(large_circle, 270)
+forward(3*large_circle//4)
+circle(large_circle//4, 90)
+forward(3*large_circle//4)
+end_fill()
+penup()
+
+goto(center[0]+small_circle, center[1]+large_circle)
+color('#3c6eb4')
+pendown()
+circle(small_circle, 160)
+color('white')
+circle(small_circle, 110)
+forward(small_circle*2)
+penup()
+
+goto(center[0]+-small_circle*2, center[1]+small_circle*2)
+color('#3c6eb4')
+pendown()
+circle(small_circle, 70)
+color('white')
+circle(small_circle, 110)
+penup()
+
+goto(center[0]+small_circle, center[1]+large_circle)
+right(-90)
+pendown()
+forward(small_circle*2)
+color('#3c6eb4')
+circle(small_circle, 90)
+color('white')
+penup()
+
+goto(center[0]+small_circle, center[1]+large_circle)
+right(90)
+pendown()
+forward(small_circle*2)
+penup()
+
+
+<<<<<<< HEAD
+# Ubuntu by Alen
+forward(500)
+pendown()
+setup(640,640)
+bgcolor('black')
+col = '#DD4814'
+R = 75
+r = 38.91*R/100
+w = 19.25*R/100
+r2 = 13.39*R/100
+r3 = 5.02*R/100
+h = 3.77*R/100
+l = 68.62*R/100
+color(col)
+up()
+fd(R)
+lt(90)
+down()
+begin_fill()
+circle(R)
+end_fill()
+up()
+color('white')
+lt(90)
+fd(R-(r+w))
+rt(90)
+down()
+begin_fill()
+circle(r+w)
+end_fill()
+up()
+lt(90)
+color(col)
+fd(w)
+rt(90)
+down()
+begin_fill()
+circle(r)
+end_fill()
+up()
+lt(90)
+fd(r)
+rt(120)
+down()
+color('white')
+for i in range(3):
+    up()
+    fd(l+r2+r3)
+    lt(90)
+    down()
+    color(col)
+    begin_fill()
+    circle(r2+r3)
+    end_fill()
+    up()
+    lt(90)
+    fd(r3)
+    rt(90)
+    color('white')
+    down()
+    begin_fill()
+    circle(r2)
+    end_fill()
+    up()
+    color('white')
+    lt(90)
+    fd(l+r2)
+    rt(90)
+    down()
+    color(col)
+    begin_fill()
+    fd(h)
+    lt(90)
+    fd(r+w)
+    lt(90)
+    fd(h*2)
+    lt(90)
+    fd(r+w)
+    lt(90)
+    fd(h)
+    end_fill()
+    up()
+    lt(30)
+
+# Draw heart
+pen = Turtle()
+def curve():
+    for i in range(200):
+        pen.right(1)
+        pen.forward(1)
+def heart():
+    pen.left(140)
+    pen.forward(113)
+    curve()
+    pen.left(120)
+    curve()
+    pen.forward(112)
+def txt():
+    pen.up()
+    pen.setpos(-200, 195)
+    pen.down()
+
+    pen.color('lightgreen')
+    pen.write("FOSSMEET'23", font=(
+      "Verdana", 12, "bold"))
+heart()
+txt()
+pen.ht()
+pen.end_fill()
+=======
+
+#turtle.Screen().bgcolor("black")
+#turtle.speed(0)
+turtle.pensize(2)
+turtle.pencolor('blue')
+
+def drawcircle(radius):
+  for i in range(10):
+    turtle.circle(radius)
+    radius=radius-4
+    
+def drawdesign():
+  for i in range(10):
+    drawcircle(150)
+    turtle.right(36)
+    
+    
+    
+drawdesign()
+#turtle.done()
+
+# Draw a Panda using Turtle Graphics
+# Import turtle package
+#import turtle
+
+# Creating a turtle object(pen)
+pen = turtle.Turtle()
+
+# Defining method to draw a colored circle
+# with a dynamic radius
+def ring(col, rad):
+
+	# Set the fill
+	pen.fillcolor(col)
+
+	# Start filling the color
+	pen.begin_fill()
+
+	# Draw a circle
+	pen.circle(rad)
+
+	# Ending the filling of the color
+	pen.end_fill()
+
+
+# Draw first ear
+pen.up()
+pen.setpos(-35, 95)
+pen.down
+ring('black', 15)
+
+# Draw second ear
+pen.up()
+pen.setpos(35, 95)
+pen.down()
+ring('black', 15)
+
+##### Draw face #####
+pen.up()
+pen.setpos(0, 35)
+pen.down()
+ring('white', 40)
+
+##### Draw eyes black #####
+
+# Draw first eye
+pen.up()
+pen.setpos(-18, 75)
+pen.down
+ring('black', 8)
+
+# Draw second eye
+pen.up()
+pen.setpos(18, 75)
+pen.down()
+ring('black', 8)
+
+##### Draw eyes white #####
+
+# Draw first eye
+pen.up()
+pen.setpos(-18, 77)
+pen.down()
+ring('white', 4)
+
+# Draw second eye
+pen.up()
+pen.setpos(18, 77)
+pen.down()
+ring('white', 4)
+
+##### Draw nose #####
+pen.up()
+pen.setpos(0, 55)
+pen.down
+ring('black', 5)
+
+##### Draw mouth #####
+pen.up()
+pen.setpos(0, 55)
+pen.down()
+pen.right(90)
+pen.circle(5, 180)
+pen.up()
+pen.setpos(0, 55)
+pen.down()
+pen.left(360)
+pen.circle(5, -180)
+#pen.hideturtle()
+
+# test
+r = Turtle()
+r.setpos(150,150)
+r.left(90)
+r.forward(100)
+r.right(90)
+r.forward(100)
+r.right(90)
+r.forward(50)
+r.right(90)
+r.forward(100)
+r.left(135)
+r.forward(90.8)
+
+
+>>>>>>> 1b178933f8ccd68a17e6e116973d74075d297991
